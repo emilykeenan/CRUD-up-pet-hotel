614252c9e5f63315022e621fac7fd4290d3da5a8
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-17 17:07:54 -0600

    initial code

---
+++ routes/pets.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/omicron';
+
+router.get('/', function(req, res) {
+  console.log('get request');
+  // get books from DB
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM visits JOIN pets ON pets.id = visits.pet_id JOIN owners ON pets.owner_id = owners.id;', function(err, result) {
+      done(); // close the connection.
+
+      console.log('the client!:', client);
+
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+
+    });
+
+  });
+});
+
+module.exports = router;
