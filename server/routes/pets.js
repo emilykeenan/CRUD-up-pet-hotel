ded9216249bad8a185877723e1ff6c9182b5ae76
jasunde <jlaurits@gmail.com>
2016-11-17 16:37:20 -0600

    Create route to insert pet

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
 
+router.put('/', function (req, res) {
+  pg.connect(connectionString, function (err, client, done) {
+    if(err) {
+      console.log('Database connection error:', err);
+      res.sendStatus(500);
+    }
 
+    client.put(
+      'INSERT INTO pets (name, breed, color, owner_id)' +
+      ' VALUES ($1, $2, $3, $4)',
+      [newPet.name, newPet.breed, newPet.color, newPet.owner_id],
+      function (err, result) {
+        done();
+
+        if(err) {
+          console.log('insert query error:',err)
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      }
+    );
+  });
+});
 module.exports = router;
